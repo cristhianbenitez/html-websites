14536669b2fd77bd2253c5e24a710074923d95ba
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-09-06 12:01:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ document.querySelector('.navbar__menu').addEventListener('click', () => {
     .querySelector('.navbar__list')
     .classList.toggle('navbar__list--toggler');
 });
-
-document.querySelector('.navbar__list--toggler');
